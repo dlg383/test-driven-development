1fe46163398618fe27129b668c3efc19205432a8
ALEJANDRO MANZANO IBAÑEZ <ami103@inlumine.ual.es>
2024-03-18 17:50:08 +0100

    Ejercicio 1

--- src/ejercicio1.py
+++ src/ejercicio1.py
@@ -3,24 +3,16 @@ class Inventario:
         self.productos = {}
 
     def añadir_producto(self, nombre, cantidad, precio):
-        self.productos[nombre] = {"cantidad": cantidad, "precio": precio}
+        pass
 
     def actualizar_producto(self, nombre, cantidad=None, precio=None):
-        if nombre in self.productos:
-            if cantidad is not None:
-                self.productos[nombre]["cantidad"] = cantidad
-            if precio is not None:
-                self.productos[nombre]["precio"] = precio
+        pass
 
     def eliminar_producto(self, nombre):
-        if nombre in self.productos:
-            del self.productos[nombre]
+        pass
 
     def obtener_producto(self, nombre):
-        if nombre in self.productos:
-            return {"nombre": nombre, **self.productos[nombre]}
-        else:
-            raise KeyError("El producto no existe")
+        pass
 
     def listar_productos(self):
-        return [{"nombre": nombre, **detalles} for nombre, detalles in self.productos.items()]
+        pass
